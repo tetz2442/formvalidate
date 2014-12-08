dcc9fd2c4f578bd063389ffa10c67a59301f5c8d
Jon Tetzlaff <tetz2442@gmail.com>
2014-12-08 08:17:57 -0600

    Map file

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
                         hoist_funs: false,
                         hoist_vars: false
                     },
+                    sourceMap: true,
                     banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
                     '<%= grunt.template.today("yyyy-mm-dd") %> */'
                 },
